c128212a99630499e37f2a6f97ea1d47d79f0bc6
Mike <leemikeholland@gmail.com>
2019-02-25 22:42:39 +0700

    bunch of oop oriented changed to the code

--- oop.js
+++ oop.js
@@ -1,89 +1,60 @@
-function initCanvas() {
-  let ctx = document.getElementById('c').getContext("2d"),
-      cw = ctx.canvas.width  = window.innerWidth,
-      ch = ctx.canvas.height = window.innerHeight,
-      animateInterval = setInterval(Animate, 30),
-      zombieSpawner = setInterval(Spawn, 1000),
-      z = [];
+var pos = {
+  x: 0,
+  y: 0,
+  vx: 0,
+  vy: 0,
+  key:[]
+}
 
-  function Animate(){
-    ctx.save();
-    ctx.clearRect(0, 0, cw, ch);
-    //Draw here
-    Player(ctx, cw, ch);
-    Zombies();
-    //Finish
-    ctx.restore();
-  }
+function Draw(){
+  let map = new Map();
+  let player = new Player(map.ctx, map.cw, map.ch);
+      
+  requestAnimationFrame(Draw);
+  map.update();
+  map.draw();
+  player.draw();
+}
 
-  function Zombies(){
-    for(let i = 0; i < z.length; i++){
-      /* zombie bites and dies */
-      if(z[i].x < z[i].px + 10 &&
-         z[i].x > z[i].px - 10 &&
-         z[i].y > z[i].py - 10 &&
-         z[i].y < z[i].py + 10    ){
-//        score -= 500;
-         z.splice(i, 1);
-      }
-      z[i].render(ctx, cw, ch);
-    }
-  }
+function Map(){
+  this.ctx = document.getElementById('c').getContext("2d");
+  this.cw = this.ctx.canvas.width  = window.innerWidth;
+  this.ch = this.ctx.canvas.height = window.innerHeight;
+  this.bg = new Image();
+  this.bg.src = "v4.png";
+  this.speed = 10;
+  this.friction = .8;
+  
+  this.update = function(){
 
-  function Spawn(){
-    z.push(new Zombie(ctx, cw, ch));
   }
+  
+  this.draw = function(){
+    this.ctx.clearRect(0, 0, this.cw, this.ch);
+    this.ctx.drawImage(this.bg, pos.x, pos.y, this.cw, this.ch, 0, 0, this.cw, this.ch);
+  };
 }
 
 function Player(ctx, cw, ch){
   this.x = cw/2;
   this.y = ch/2;
-  this.radius = 10,
-  this.sAngle = 0,
-  this.eAngle = Math.PI * 2,
+  this.radius = 10;
+  this.sAngle = 0;
+  this.eAngle = Math.PI * 2;
   this.color = "black";
-
-  ctx.fillStyle = this.color;
-  ctx.beginPath();
-  ctx.arc(this.x, this.y, this.radius, this.sAngle, this.eAngle);
-  ctx.fill();
-}
-
-function Random(min, max){
-  return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
-
-function Zombie(ctx, cw, ch){
-  this.px = cw/2,
-  this.py = ch/2;
-  this.x = Random(-this.px * 5, this.px * 5),
-  this.y = Random(-this.py * 5, this.py * 5),
-  this.radius = 5,
-  this.sAngle = 0,
-  this.eAngle = Math.PI * 2,
-  this.color = "green";
-
-  this.render = function(){
-    this.x -= (Random(0, this.x) - Random(0, this.px)) * .01;
-    this.y -= (Random(0, this.y) - Random(0, this.py)) * .01;
-
-  /* rushes at player */
-   if(this.x < this.px + 300 &&
-      this.x > this.px - 300 &&
-      this.y < this.py + 300 &&
-      this.y > this.py - 300    ){
-
-      this.x -= (this.x - this.px) * .04;
-      this.y -= (this.y - this.py) * .04;
-    }
-
+  
+  this.draw = function(){
     ctx.fillStyle = this.color;
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, this.sAngle, this.eAngle);
     ctx.fill();
-  }
+  };
+}
+
+function random(min, max){
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 window.addEventListener('load', function(event){
-  initCanvas();
-})
+  Draw();
+});
