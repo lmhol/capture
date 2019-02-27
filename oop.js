18a074c6ae38c9a513f1ee05fba769552fe30494
Mike <leemikeholland@gmail.com>
2019-02-27 22:07:07 +0700

    rudimentary bullets

--- oop.js
+++ oop.js
@@ -3,17 +3,20 @@ var pos = {
   y: 0,
   vx: 0,
   vy: 0,
-  key:[]
+  key:[],
+  b: []
 }
 
 function draw(){
   let map = new Map();
-  let player = new Player(map.ctx, map.cw, map.ch);
+  let player = new Thing(map.ctx, map.cw, map.ch);
+  let bullet = new Thing(map.ctx)
       
   requestAnimationFrame(draw);
   map.update();
   map.draw();
   player.draw();
+  bullet.shoot();
 }
 
 function Map(){
@@ -21,7 +24,7 @@ function Map(){
   this.cw = this.ctx.canvas.width  = window.innerWidth;
   this.ch = this.ctx.canvas.height = window.innerHeight;
   this.bg = new Image();
-  this.bg.src = "v5.png";
+  this.bg.src = "v4.png";
   this.speed = 10;
   this.friction = .8;
   
@@ -59,32 +62,45 @@ function Map(){
   };
 }
 
-function Player(ctx, cw, ch){
-  this.x = cw/2;
-  this.y = ch/2;
-  this.radius = 10;
+function Thing(ctx, x, y){
   this.sAngle = 0;
   this.eAngle = Math.PI * 2;
-  this.color = "black";
   
   this.draw = function(){
-    ctx.fillStyle = this.color;
+    ctx.fillStyle = "black";
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, this.sAngle, this.eAngle);
+    ctx.arc(x/2, y/2, 10, this.sAngle, this.eAngle);
     ctx.fill();
+    ctx.closePath();
   };
+  
+  this.shoot = function(){
+    if(pos.b.length > 0){
+      for(var i = 0; i < pos.b.length; i++){
+        ctx.fillStyle = "gold";
+        ctx.beginPath();
+        ctx.arc(pos.b[i][0], pos.b[i][1], 3, this.sAngle, this.eAngle);
+        ctx.fill();    
+        ctx.closePath();
+      }
+    }
+  }
 }
 
 function random(min, max){
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+window.addEventListener("click", function (e) { 
+  pos.b.push([e.clientX,e.clientY, pos.x, pos.y]);
+ });
+
 window.addEventListener("keydown", function (e) {
-  pos.key[e.keyCode] = true;
+pos.key[e.keyCode] = true;
 });
 
 window.addEventListener("keyup", function (e) {
-  pos.key[e.keyCode] = false;
+pos.key[e.keyCode] = false;
 });
 
 window.addEventListener('load', draw);
